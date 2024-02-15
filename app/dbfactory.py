9260c934bc1830b64d7f64e240845e487fb8ea26
YeEul, Kwon <gruuriii@gmail.com>
2024-02-15 16:54:48 +0900

    product router 추가 header - cart, product 경로 지정 db 생성 추가

--- app/dbfactory.py
+++ app/dbfactory.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.settings import config
-from app.models import member
+from app.models import member, product
 
 # sqlite 사용시 check_same_thread를 추가 - 쓰레드 사용 안함
 engine = create_engine(config.db_conn, echo=True, connect_args={'check_same_thread':False})
@@ -10,5 +10,5 @@ Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 서버시작시 테이블 생성
 def db_startup():
-
-    member.Base.metadata.create_all(engine)
+    # member.Base.metadata.create_all(engine)
+    product.Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -5,14 +7,25 @@ from fastapi.staticfiles import StaticFiles
 
 from app.dbfactory import db_startup
 from app.routes.board import board_router
+from app.routes.cart import cart_router
 from app.routes.member import member_router
+from app.routes.product import product_router
 
-app = FastAPI()
+# 서버시작시 디비 생성
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    db_startup()
+
+app = FastAPI(lifespan=lifespan)
 
 # jinja2 설정
 templates = Jinja2Templates(directory='views/templates')
 app.mount('/static', StaticFiles(directory='views/static'), name='static')
 
+# 외부 라우터 불러오기
+app.include_router(product_router, prefix='/shops/product')
+app.include_router(cart_router, prefix='/shops')
 
 @app.get("/", response_class=HTMLResponse)
 async def index(req: Request):
